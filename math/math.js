cdc6a5126f1e88ae54a973a52bd9e90d2ccb0a30
unknown <wassoval000@warren.k12.in.us>
2019-01-29 13:08:34 -0500

    first

---
+++ math/math.js
@@ -0,0 +1,66 @@
+$().ready(function(){
+
+    $('#convert').click(function(){
+        convert();
+    });
+
+    $('#total').click(function(){
+        total();
+    });
+
+    $('#BMI').click(function(){
+        BMI();
+    });
+
+});
+
+var convert = function(){
+
+    var min = $('#numMin').val();
+
+    var years;
+    var daysRem;
+    var days;
+
+    years = Math.floor(min/525600);
+    daysRem = min%525600;
+    days = Math.floor(daysRem/(60*24));
+
+    $('#output').text("Results: " + years + " years, " + days + "days");
+
+}
+
+var total = function(){
+
+    var grat = $('#gratuity').val();
+    var sub = $('#subtotal').val();
+
+    sub = parseInt(sub);
+    var dollarGra = grat*sub;
+    var total = sub+dollarGra;
+
+    $('#output2').text("Results: Gratuity: " + dollarGra + ", Total: " + total);
+}
+
+var BMI = function(){
+
+    var pounds = $('#pounds').val();
+    var inches = $('#inches').val();
+
+    var kilo;
+    var meter;
+    var metersquare;
+    var result;
+    
+    kilo = pounds/2.2;
+    meter = inches/39.3701;
+    metersquare = Math.pow(39.3701,2);
+    result = kilo/metersquare;
+
+    $('#output3').text("Results: BMI is " + result);
+
+}
+
+var time = function(){
+    
+}
